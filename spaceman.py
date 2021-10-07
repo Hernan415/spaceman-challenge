823717cb8c5e52cb64ed003c42bb38b2cea6b548
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-10-07 10:29:09 -0700

    Update spaceman.py

--- spaceman.py
+++ spaceman.py
@@ -39,7 +39,12 @@ def get_guessed_word(secret_word, letters_guessed):
     '''
 
     #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
-
+    #make word into _s
+    #append if letter is correct
+    while attempts <= len(secret_word):
+        secret_word.replace(" " , "_")
+        if guess in secret_word:
+            print(secret_word.replace('_', guess))
     pass
 
 
@@ -53,9 +58,7 @@ def is_guess_in_word(guess, secret_word):
         bool: True if the guess is in the secret_word, False otherwise
     '''
     #TODO: check if the letter guess is in the secret word
-
-    pass
-
+    return guess in secret_word
 
 
 
@@ -68,19 +71,20 @@ def spaceman(secret_word):
 
     attempts = len(secret_word)
     #TODO: show the player information about the game according to the project spec
-    print("Hello player your secret word has " + str(len(secret_word)) + " letters and you have" + str(len(secret_word)) + "attempts to complete this game")
+    print("Hello player, your secret word has " + str(len(secret_word)) + " letters and you have " + str(len(secret_word)) + " attempts to complete this game.")
     #TODO: Ask the player to guess one letter per round and check that it is only one letter
     while attempts <= len(secret_word):
         guess = input("Please enter a single letter: ")
         if len(guess) > 1:
             print("Please only choose one letter and not more than one, please try again.")
-    #TODO: Check if the guessed letter is in the secret or not and give the player feedback
-    if guess in secret_word:
-        print("You are correct please continue")
-
-    else:
-        attempts = attempts + 1
-        print("sorry you are incorrect you have " + attempts + "attempts left.")
+        #TODO: Check if the guessed letter is in the secret or not and give the player feedback
+        if is_guess_in_word(guess, secret_word):
+            print("You are correct please continue")
+            get_guessed_word
+        else:
+            attempts = attempts - 1
+            print("sorry you are incorrect you have " + str(attempts) + " attempts left.")
+            get_guessed_word
     #TODO: show the guessed word so far
 
     #TODO: check if the game has been won or lost
